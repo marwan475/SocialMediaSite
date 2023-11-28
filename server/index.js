d103f2a013d94bbcffaf8b9786eb77ee0688f886
Marwan Mostafa <mam024@usask.com>
2023-11-28 16:09:33 -0600

    Db conection added

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "root"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Db Connected!");
+  });
+
 const server = app.listen(8080, ()=>{
     console.log("Server started");
 })
