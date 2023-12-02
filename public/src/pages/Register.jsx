2eaacf7e4c92abb124f6595782af7a56452c225a
Marwan Mostafa <mam024@usask.com>
2023-12-02 17:05:46 -0600

    completed front end design of Register page

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled  from "styled-components";
+import Logo from "../assets/logo.svg";
 
 function Register() {
 
@@ -16,9 +17,9 @@ function Register() {
     return( 
     <>
         <FormContainer>
-            <form onSubmit={(event)=>handleSubmit(event)}>
+            <form className="submitForm" onSubmit={(event)=>handleSubmit(event)}>
                 <div className="webName">
-                    <img src="" />
+                    <img src={Logo} alt="Logo" />
                     <h1>Twisted Tech Questions</h1>
                 </div>
                 <input 
@@ -34,7 +35,7 @@ function Register() {
                 onChange={(e)=>handleChange(e)}
                 />
                 <button type="submit">Register</button>
-                <span>Have an account? <Link to="/login">Login</Link></span>
+                <span>Have an account? <br></br><Link to="/login">Login</Link></span>
             </form>
         </FormContainer>
     </>
@@ -42,6 +43,59 @@ function Register() {
    
 }
 
-const FormContainer =styled.div``;
+const FormContainer =styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    align-items; center;
+    background-color: #A1A1A1;
+    padding: 15% ;
+    .webName{
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        justify-content: center;
+        img {
+            height: 5rem;
+        }
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        background-color: #00000076;
+        border-radius: 2rem;
+        padding: 3rem 5rem;
+        input {
+            text-indent: 0.5rem;
+            background-color: transparent;
+            padding: 1rem
+            border: 0.1rem solid #4e0eff;
+            border-radius: 0.4rem;
+            color: white;
+            width: 100%;
+            font-size: 1rem;
+            &:focus {
+                border: 0.1rem solid #997af0;
+                outline: none;
+                &::placeholder {
+                    color: transparent;
+                }
+            }
+        }
+        button {
+            padding: 1rem 2rem;
+            border: none;
+            cursor: pointer;
+            border-radius: 0.4rem;
+        }
+        span {
+            text-align: center;
+        }
+    }
+`;
 
 export default Register;
